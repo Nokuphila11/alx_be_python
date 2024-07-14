e0dacb0c033d76f99cd0841572d5be7e9caa2668
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-07-14 14:10:20 +0000

    add basic operations file

--- python_introduction/basic_operation.py
+++
@@ -1,8 +0,0 @@
-number1 = 10
-number2 = 5
-print(f"{number1}")
-print(f"{number2}")
-result = number1 + number2
-result = number1 - number2
-result = number1 * number2
-print(f"{result}")
---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+print(f"{number1}")
+print(f"{number2}")
+result = number1 + number2
+result = number1 - number2
+result = number1 * number2
+print(f"{result}")
