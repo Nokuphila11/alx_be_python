526ae26f6ae708cae40c552c4909c37dfc97de6d
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-07-15 20:27:24 +0200

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-age = input("How old are you?")
+ age = input("How old are you?")
 num1 = int(input("27"))
 num2 = int(input("30"))
-print("2050 is: " + 57)
+print("In 2050:"you will be 57 years old)
