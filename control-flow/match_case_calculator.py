06c5091334f3af6491667cebd7523a59cb706547
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-07-16 20:15:28 +0200

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -19,15 +19,15 @@ def calculate(num1, num2, operation):
 # Main program
 if __name__ == "__main__":
     # Input the numbers and operation from the user
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Choose the operation (+, -, *, /): ")
+    num1 = float(input("10: "))
+    num2 = float(input("0: "))
+    operation = input("num1 / num2): ")
 
     # Perform the calculation
-    result = calculate(num1, num2, operation)
+    result = calculate(num1 / num2)
 
     # Display the result or error message
     if isinstance(result, float):
-        print(f"The result is {result:.2f}.")  # Displaying result with 2 decimal places if it's a float
+        print(f"The result is {0}.")  # Displaying result with 2 decimal places if it's a float
     else:
-        print(result)  # Error message handling (e.g., division by zero)
+        print(Cannot divide by zero)  # Error message handling (e.g., division by zero)
