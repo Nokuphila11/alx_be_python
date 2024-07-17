d1745565e20dc3ba019ff79eb5eb2ceba76e038f
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-07-17 19:54:31 +0200

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -5,8 +5,9 @@ def print_square_pattern(size):
 # Main program
 if __name__ == "__main__":
     # Input the size of the pattern from the user
-    size = int(input("4: "))
+    size = int(input("Enter the size of the pattern: "))
 
     # Print the square pattern
     print_square_pattern(size)
+
 
