9216c298ffd2ae77de7195d419bedf300f5dbf01
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-07-17 20:41:55 +0200

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,6 +3,10 @@
     priority = input("Priority (high/medium/low): ").strip().lower()
     time_bound = input("Is it time-bound? (yes/no): ").strip().lower()
 
+ task = input("Finish project report: ")
+    priority = input("High Priority): ").strip().lower()
+    time_bound = input("Is it time-bound? (yes): ").strip().lower()
+
     # Generate and print the reminder message
     reminder =(Finish project report, is a high priority, that requires attention today)
     print(f"\nReminder: {reminder}")
